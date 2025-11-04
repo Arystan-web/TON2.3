a97d847a5723a9b3797b31d6b684aab06fbcc9a6
Arystan-web <ansar.ben10@mail.ru>
2025-11-04 17:51:26 +0500

    Update server.js

--- server.js
+++ server.js
@@ -160,4 +160,7 @@ if (!BOT_TOKEN) {
   })();
 }
 
-app.listen(PORT, ()=> console.log(`✅ Server running on port ${PORT}`));
+app.listen(process.env.PORT || 10000, '0.0.0.0', () =>
+  console.log(`✅ Server running on port ${process.env.PORT || 10000}`)
+);
+
